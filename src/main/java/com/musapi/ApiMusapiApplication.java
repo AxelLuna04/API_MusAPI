aeb8614675f1235cd1886f295ce468ecfcae41b0
Jarlocho <jarlyhdezromero@hotmail.com>
2025-06-26 20:33:06 -0600

    Cambios para gRPC

--- src/main/java/com/musapi/ApiMusapiApplication.java
+++ src/main/java/com/musapi/ApiMusapiApplication.java
@@ -1,9 +1,14 @@
 package com.musapi;
 
+import net.devh.boot.grpc.server.autoconfigure.GrpcServerSecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+
+@SpringBootApplication(exclude = {
+        GrpcServerSecurityAutoConfiguration.class
+})
 public class ApiMusapiApplication {
 
 	public static void main(String[] args) {
