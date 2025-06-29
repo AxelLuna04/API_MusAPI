ad18b0cfbea85334f01c74ec8db72a3e2e376a72
Axel Luna Hernandez <axel.lu04@gmail.com>
2025-06-28 19:33:19 -0600

    Ultimos cambios

--- src/main/java/com/musapi/grpc/CategoriaMusicalGrpcService.java
+++ src/main/java/com/musapi/grpc/CategoriaMusicalGrpcService.java
@@ -6,7 +6,10 @@ package com.musapi.grpc;
 
 import com.musapi.model.CategoriaMusical;
 import com.musapi.repository.CategoriaMusicalRepository;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
+import java.util.List;
+import java.util.Optional;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -37,5 +40,51 @@ public class CategoriaMusicalGrpcService extends CategoriaMusicalServiceGrpc.Cat
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
+    
+    @Override
+    public void obtenerCategoriasMusicales(Empty request, StreamObserver<CategoriaMusicalListResponse> responseObserver) {
+        List<CategoriaMusical> categorias = categoriaMusicalRepository.findAll();
+
+        CategoriaMusicalListResponse.Builder responseBuilder = CategoriaMusicalListResponse.newBuilder();
+        for (CategoriaMusical cat : categorias) {
+            CategoriaMusicalResponse item = CategoriaMusicalResponse.newBuilder()
+                .setIdCategoriaMusical(cat.getIdCategoriaMusical())
+                .setNombre(cat.getNombre())
+                .setDescripcion(cat.getDescripcion())
+                .build();
+            responseBuilder.addCategorias(item);
+        }
+
+        responseObserver.onNext(responseBuilder.build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void editarCategoriaMusical(CategoriaMusicalEditRequest request, StreamObserver<CategoriaMusicalResponse> responseObserver) {
+        Optional<CategoriaMusical> optional = categoriaMusicalRepository.findById(request.getIdCategoriaMusical());
+
+        if (!optional.isPresent()) {
+            responseObserver.onError(Status.NOT_FOUND
+                .withDescription("Categoría no encontrada")
+                .asRuntimeException());
+            return;
+        }
+
+        CategoriaMusical existente = optional.get();
+        existente.setNombre(request.getNombre());
+        existente.setDescripcion(request.getDescripcion());
+
+        CategoriaMusical actualizada = categoriaMusicalRepository.save(existente);
+
+        CategoriaMusicalResponse response = CategoriaMusicalResponse.newBuilder()
+            .setIdCategoriaMusical(actualizada.getIdCategoriaMusical())
+            .setNombre(actualizada.getNombre())
+            .setDescripcion(actualizada.getDescripcion())
+            .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
 }
 
